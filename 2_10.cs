7f0b22faf416b4fd318b5c8ba9dc78c0481d141c
Luis2ceron <111613743+Luis2ceron@users.noreply.github.com>
2022-11-12 09:31:02 -0500

    Add files via upload

---
+++ 2_10.cs
@@ -0,0 +1,16 @@
+﻿namespace Ejemplos
+{
+    public class ejercicio2_10
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Inserte la cantidad de metros que necesita");
+            double metros_Necesarios = (Convert.ToInt32(Console.ReadLine()));
+
+            double pulgadas_Necesarias =Math.Truncate( metros_Necesarios / 0.0254);
+            Console.WriteLine("la cantidad de pulgadas que debe pedir son " + pulgadas_Necesarias );
+        }
+
+
+    }
+}
---
+++ 2_7.cs
@@ -0,0 +1,22 @@
+﻿namespace Ejemplos
+{ 
+    public class ejercicio2_10
+{
+    public static void Main()
+    {
+        Console.WriteLine("Inserte la cantidad de litros");
+        double litros_vendidos = (Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("Inserte el valor por galon");
+            double valorGalon = (Convert.ToInt32(Console.ReadLine()));
+
+            double galones_vendidos = Math.Truncate(litros_vendidos / 3.785);
+            double pagoGalon = (Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine("la cantidad de leche en galones es  " + galones_vendidos);
+         Console.WriteLine("la cantidad de leche en galones es  " + galones_vendidos);
+        }
+
+
+}
+}
